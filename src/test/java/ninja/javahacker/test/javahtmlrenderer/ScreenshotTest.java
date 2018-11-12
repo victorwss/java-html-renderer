d7c4f712386fdc9d0d767abab8c15c0318f53670
Victor Williams Stafusa da Silva <vstafusa@prefeitura.sp.gov.br>
2018-11-12 17:23:34 -0200

    Small fixes.

--- src/test/java/ninja/javahacker/test/javahtmlrenderer/ScreenshotTest.java
+++ src/test/java/ninja/javahacker/test/javahtmlrenderer/ScreenshotTest.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ninja.javahacker.test.javahtmlrenderer;
 
 import java.awt.EventQueue;
 import java.awt.image.BufferedImage;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.swing.JButton;
